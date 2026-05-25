b0f873d13f5a1ac7c0cf1e48c08d5d9aeffda22b
IrynaPolovko <pinguin5771@gmail.com>
2026-05-25 20:28:13 +0300

    Train.cpp

--- ConsoleApplication1/Train.cpp
+++ ConsoleApplication1/Train.cpp
@@ -1,1 +1,50 @@
 #include "Train.h"
+
+Train::Train()
+{
+    number = "undefined";
+    departure = "undefined";
+    destination = "undefined";
+}
+
+Train::Train(string number, string departure, string destination)
+{
+    this->number = number;
+    this->departure = departure;
+    this->destination = destination;
+}
+
+string Train::getNumber() const
+{
+    return number;
+}
+
+string Train::getDeparture() const
+{
+    return departure;
+}
+
+string Train::getDestination() const
+{
+    return destination;
+}
+
+void Train::setNumber(string number)
+{
+    this->number = number;
+}
+
+void Train::setDeparture(string departure)
+{
+    this->departure = departure;
+}
+
+void Train::setDestination(string destination)
+{
+    this->destination = destination;
+}
+
+void Train::showInfo() const
+{
+    cout << number << " " << departure << " " << destination << endl;
+}
